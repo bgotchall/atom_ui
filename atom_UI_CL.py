a76285811d75df7e6cbe8e39bdfb4d540b84b789
Bob Gotchall <bob.g@sigmasense.com>
2023-12-21 14:09:02 -0600

    more stuff

--- atom_UI_CL.py
+++ atom_UI_CL.py
@@ -1,26 +1,69 @@
 import telnetlib
+#import asyncio, telnetlib3
 import time
 #import getpass
 import subprocess
 import pyvisa
 
- 
+
+
+
+#################################################
+# actual rudimentary UI that just runs the tester for the one "tray"
+#Load program
+#(operator hits the start button)
+#If (RFT=0):
+#               Start test flow
+#               Get test result
+#               Set bin1 or bin 2
+#End if
 rm=pyvisa.ResourceManager()
-print(rm.list_resources())
-my_instrument = rm.open_resource('GPIB0::4::INSTR')
-print(my_instrument.query('*IDN?'))
-print(my_instrument.query('RFT?'))          # zero means yes, a part is reaady.  1 means no
-#print(my_instrument.query('BIN1'))         # this works.  tells the handler to move on.
-time.sleep(1)
-print(my_instrument.query('RFT?')) 
-time.sleep(1)
-print(my_instrument.query('RFT?')) 
+print("attempting to open GPIB")
+try:
+    print("GPIB resources available: ",rm.list_resources())
+    my_instrument = rm.open_resource('GPIB0::4::INSTR')
+    print("UI starting.  The program should be loaded and the correct temperature set.  Enter to continue")
+    a=input()
+    print("Enter name of .STDF logfile")
+    a=input()
+    print(a)
+
+
+
+
+    
+except:
+    print("No GPIB or some other error.")
+
+
+
+######################################################
+
+if 0: 
+    rm=pyvisa.ResourceManager()
+    print(rm.list_resources())
+    my_instrument = rm.open_resource('GPIB0::4::INSTR')
+    print(my_instrument.query('*IDN?'))
+    print(my_instrument.query('RFT?'))          # zero means yes, a part is ready.  1 means no
+    #print(my_instrument.query('BIN1'))         # this works.  tells the handler to move on.
+    time.sleep(1)
+    print(my_instrument.query('RFT?')) 
+    time.sleep(1)
+    print(my_instrument.query('RFT?')) 
 
 if 0:
     subprocess.run(["ls","-l"])
     subprocess.run(["ateliercmd", "-getrevision"])
 
 ############################################
+#HOST ="www.sigmasense.com"
+#tn=telnetlib.Telnet(HOST,"80")
+#tn.write(b"ping\n")
+#print(tn.read_all())
+
+
+
+
 
 if 0:
     HOST = "10.109.10.20, port=5000"
@@ -38,13 +81,14 @@ if 0:
     #print("interact")
     #tn.interact()
 
-    if 1:
+    if 0:
         print("writing the command")
         #tn.write(user.encode('ascii') + b"mMI0699?\n")
         tn.write(b"mMI0699?\n")
-        #print(tn.read_all())
+        
         print ("attempting to read")
-        print(tn.read_until(b"MI0699"),4)
+        print(tn.read_all())
+        #print(tn.read_until(b"MI0699"),4)
     
 
 
